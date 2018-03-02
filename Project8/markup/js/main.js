253933895bbe3000d3ad9388fabed3ff20ebbb25
Елена Киктева <elena.kikteva88@gmail.com>
2018-03-02 19:02:20 +0200

    point in the shaded region

---
+++ Project8/markup/js/main.js
@@ -0,0 +1,24 @@
+var x = 0;
+var y = 0;
+do {
+    alert('Enter the coordinates of the point');
+    x = prompt('Input x: ', '');
+    x = parseInt(x);
+    if (isNaN(x)) {
+        alert('Error. "x" should be number');
+    } else {
+        y = prompt('Input y: ', '');
+        y = parseInt(y);
+        if (isNaN(y)) {
+            alert('Error. "y" should be number');
+        }
+    }
+    console.log(isNaN(x));
+} while(isNaN(x) || isNaN(y));
+if ( x <= 0 && x >= -2 && y >= -(x+2) || x*x + y*y <=1){
+    alert('The point is in the shaded region.');
+}else {
+    alert('The point is not in the shaded region.');
+}
+
+console.log(x, y);
